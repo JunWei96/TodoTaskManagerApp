80fba5d560ebc4b7dde695b4a7fdcd0a2df5b18f
JunWei96 <kjunwei1996@gmail.com>
2017-12-23 21:33:11 +0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #returns the full title of the page
+  def full_title(page_title = '')
+    base_title = "TODO Manager App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
